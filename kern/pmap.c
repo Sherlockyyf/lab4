018171f9187782e9c4ef7e1fae8c1a8516cc3552
Sherlockyyf <774244238@qq.com>
2020-12-09 18:47:22 +0800

    panic

--- kern/pmap.c
+++ kern/pmap.c
@@ -298,6 +298,7 @@ page_alloc(int alloc_flags)
 	struct PageInfo *free_page;
 	// If page_free_list is NULL
 	if(!page_free_list){
+		cprintf("page_free_list is NULL!\n");
 		return NULL;
 	}
 	//
@@ -322,8 +323,11 @@ page_free(struct PageInfo *pp)
 	// Fill this function in
 	// Hint: You may want to panic if pp->pp_ref is nonzero or
 	// pp->pp_link is not NULL.
-	if (pp->pp_ref != 0 || !pp->pp_link){
-		panic("pp->pp_ref is nonzero or pp->pp_link is not NULL");
+	if (pp->pp_ref != 0 ){
+		panic("pp->pp_ref is nonzero!\n");
+	}
+	else if(!pp->pp_link){
+		panic("pp->pp_link is not NULL!\n");
 	}
 	else{
 		pp->pp_link = page_free_list;
